e26190d7dd5ef7b2b62290b8ecd29541aebfa310
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-07-31 17:51:29 -0400

    Agregada el método para el navegar al eje seleccionado

--- src/pages/ejes/ejes.ts
+++ src/pages/ejes/ejes.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {EjePage} from '../eje/eje';
+
 
 @Component({
   selector: 'page-ejes',
   templateUrl: 'ejes.html'
 })
 export class EjesPage {
-
   constructor(public navCtrl: NavController) {
-
   }
 
+  itemSeleccionado(event, ejeN) {
+    console.log(ejeN);
+    this.navCtrl.push(EjePage, {
+        eje: ejeN
+      }
+    );
+  }
 }
 
