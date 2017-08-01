bbd17ffbd66a13d8026b922145c084a6d0b05f12
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-07-31 22:12:16 -0400

    agregado contenido inicial de la pagina eje

--- src/pages/eje/eje.ts
+++ src/pages/eje/eje.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Http} from '@angular/http'; //Service to handle requests. HTTP calls returns observable of HTTP Responses (Observable<Response>)
+import 'rxjs/add/operator/map'; //Reactive Extensions Library for JavaScript
 
 /**
  * Generated class for the EjePage page.
@@ -13,8 +15,20 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'eje.html',
 })
 export class EjePage {
+  ejes = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private http: Http, public navCtrl: NavController, public navParams: NavParams) {
+    this.http.get('assets/data/ejes&lineas.json')
+      .map(res => res.json().filter((item) => {
+        return item.slug === navParams.get('eje')
+      }))
+      .subscribe(
+        data => {
+          this.ejes = data;
+        },
+        err => console.log("error es " + err), // error
+        () => console.log('Lectura de los ejes y lineas completadas ' + this.ejes) // complete
+      );
   }
 
   ionViewDidLoad() {
