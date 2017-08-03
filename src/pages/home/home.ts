571c3b1351695a49f8bd352ea4608a9ac38560d6
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-08-03 14:58:31 -0400

    Agregada la función itemSeleccionado para manejar el ejecto click de los botones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {EjesPage} from '../ejes/ejes';
+import { AccionesPage } from '../acciones/acciones';
+import { ConstruccionPage } from '../construccion/construccion';
+import {InvitarPage} from "../invitar/invitar";
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +15,32 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  itemSeleccionado(opcion) {
+    switch(opcion) {
+      case "ejes": {
+        console.log(opcion);
+        this.navCtrl.push(EjesPage);
+        break;
+      }
+      case "acciones":{
+        console.log(opcion);
+        this.navCtrl.push(AccionesPage);
+        break;
+      }
+      case "construccion":{
+        console.log(opcion);
+        this.navCtrl.push(ConstruccionPage);
+        break;
+      }
+      case "invitar":{
+        console.log(opcion);
+        this.navCtrl.push(InvitarPage);
+        break;
+      }
+      default:{
+        console.log("opción seleccionada en la pagina home es incorrecta");
+        break;
+      }
+    }
+  }
 }
