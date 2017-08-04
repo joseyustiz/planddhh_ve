ce4b17c664c54f615d6019dc973d5d0ea81f2478
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-08-04 16:15:01 -0400

    Agregada la logica para ir a la línea estratégica seleccionada y monstrar la data cargada dinamicamente del JSON

--- src/pages/eje/eje.ts
+++ src/pages/eje/eje.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Http} from '@angular/http'; //Service to handle requests. HTTP calls returns observable of HTTP Responses (Observable<Response>)
 import 'rxjs/add/operator/map'; //Reactive Extensions Library for JavaScript
+import {LineaPage} from '../linea/linea';
 
 /**
  * Generated class for the EjePage page.
@@ -15,24 +16,35 @@ import 'rxjs/add/operator/map'; //Reactive Extensions Library for JavaScript
   templateUrl: 'eje.html',
 })
 export class EjePage {
-  ejes = [];
+  eje: any;
 
   constructor(private http: Http, public navCtrl: NavController, public navParams: NavParams) {
-    this.http.get('assets/data/ejes&lineas.json')
-      .map(res => res.json().filter((item) => {
-        return item.slug === navParams.get('eje')
-      }))
+    this.http.get('assets/data/ejes.json')
+      .map(res => {
+        return res.json().ejes.filter((item) => {
+          return item.numero === navParams.get('eje')
+        })
+      })
       .subscribe(
         data => {
-          this.ejes = data;
+          this.eje = data;
         },
         err => console.log("error es " + err), // error
-        () => console.log('Lectura de los ejes y lineas completadas ' + this.ejes) // complete
+        () => console.log('Lectura de los ejes y lineas completadas ' + this.eje.toString()) // complete
       );
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EjePage');
+  }
+  lineaSeleccionada(linea){
+    this.navCtrl.push(LineaPage, {
+        linea: linea
+      }
+    );
+      console.log(linea);
+
   }
 
 }
