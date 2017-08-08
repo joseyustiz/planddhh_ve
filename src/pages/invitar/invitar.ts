08503397341f419b955269b39fcc29387ff9fe83
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-08-07 22:19:49 -0400

    Creado los botones y lógica para compartir el plan en PDF, App y Gaceta

--- src/pages/invitar/invitar.ts
+++ src/pages/invitar/invitar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {SocialSharing} from '@ionic-native/social-sharing';
 
 /**
  * Generated class for the InvitarPage page.
@@ -14,11 +15,44 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class InvitarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private socialSharing: SocialSharing) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InvitarPage');
+  }
+  itemSeleccionado(opcion) {
+    switch(opcion) {
+      case "pdf": {
+        console.log(opcion);
+        this.compartirAccion("Descarga PDF del Plan Nacional de DDHH 2016-2019","Invitación a descargar PDF del Plan DDHH",null,"http://consejoderechoshumanos.gob.ve/wp-content/uploads/2015/07/plan_Nacional_Derechos_Humanos.pdf");
+        break;
+      }
+      case "app":{
+        console.log(opcion);
+        this.compartirAccion("Descarga la Aplicación Móvil del Plan Nacional de DDHH 2016-2019","Invitación a descargar Aplicación Móvil del Plan DDHH",null,"http://consejoderechoshumanos.gob.ve/wp-content/uploads/2015/07/plan_Nacional_Derechos_Humanos.pdf");
+        break;
+      }
+      case "gaceta":{
+        console.log(opcion);
+        this.compartirAccion("Descarga Gaceta Oficial del Plan Nacional de DDHH 2016-2019","Invitación a descargar la Gaceta Oficial del Plan DDHH",null,"http://consejoderechoshumanos.gob.ve/wp-content/uploads/2015/07/plan_Nacional_Derechos_Humanos.pdf");
+        break;
+      }
+      default:{
+        console.log("opción seleccionada en la página invitar es incorrecta");
+        break;
+      }
+    }
+  }
+
+  compartirAccion(accion, titulo, archivo, url) {
+    this.socialSharing.share(accion, titulo, archivo, url)
+      .then(() => {
+        console.log("shareSheetShare: Success");
+      }).catch(() => {
+      console.error("shareSheetShare: failed");
+    });
   }
 
 }
