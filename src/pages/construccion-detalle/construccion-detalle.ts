2a31f7ad879f86ac0a90b55d25f19a38820934c7
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-08-22 23:10:46 -0400

    Agregada la lógica para mostrar el contenido del artículo de construcción del plan

--- src/pages/construccion-detalle/construccion-detalle.ts
+++ src/pages/construccion-detalle/construccion-detalle.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Http} from '@angular/http'; //Service to handle requests. HTTP calls returns observable of HTTP Responses (Observable<Response>)
 
 /**
  * Generated class for the ConstruccionDetallePage page.
@@ -13,8 +14,25 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'construccion-detalle.html',
 })
 export class ConstruccionDetallePage {
+  articulo:any;
+  etiquetas:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(private http: Http,public navCtrl: NavController, public navParams: NavParams) {
+    this.articulo=navParams.get("articulo");
+    this.http.get('assets/data/filtros.json')
+      .map(res => {
+        return res.json().filtros.filter((item) => {
+          return this.articulo.filtros.indexOf(item.slug)>=0
+        })
+      })
+      .subscribe(
+        data => {
+          this.etiquetas = data;
+        },
+        err => console.log("error es " + err), // error
+        () => console.log('Lectura de las etiquetas ' + this.etiquetas.toString()) // complete
+      );
   }
 
   ionViewDidLoad() {
