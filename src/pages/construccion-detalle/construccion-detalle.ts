4d7b7fd716c2cf10b4a99c4b38d82c10fa766849
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-09-05 17:18:37 -0400

    Contador de Google Analytics que sigue las veces vista la pág. del detalle del artículos de la construccion del plan

--- src/pages/construccion-detalle/construccion-detalle.ts
+++ src/pages/construccion-detalle/construccion-detalle.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 // import {Http} from '@angular/http'; //Service to handle requests. HTTP calls returns observable of HTTP Responses (Observable<Response>)
+import { Platform } from 'ionic-angular';
+import { GoogleAnalytics } from '@ionic-native/google-analytics';
 import { IonicPage } from 'ionic-angular';
 @IonicPage()
 
@@ -20,8 +22,10 @@ export class ConstruccionDetallePage {
   //etiquetas:any;
 
 
-  constructor(/*private http: Http,*/public navCtrl: NavController, public navParams: NavParams) {
+  constructor(/*private http: Http,*/public navCtrl: NavController, public navParams: NavParams, platform: Platform,  ga: GoogleAnalytics) {
     this.articulo=navParams.get("articulo");
+    //Google Analytics
+    platform.ready().then(() => { ga.trackView("Detalle Art. Construcción: "+this.articulo.titulo); });
     // this.http.get('assets/data/filtros.json')
     //   .map(res => {
     //     return res.json().filtros.filter((item) => {
