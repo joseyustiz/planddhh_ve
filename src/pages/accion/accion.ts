798de0054455bc32e600ca25179629248e9e1763
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2017-09-05 17:16:35 -0400

    Contador de Google Analytics que sigue las veces vista la pág. del detalle de acción programática

--- src/pages/accion/accion.ts
+++ src/pages/accion/accion.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Http} from '@angular/http'; //Service to handle requests. HTTP calls returns observable of HTTP Responses (Observable<Response>)
 import { IonicPage } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { GoogleAnalytics } from '@ionic-native/google-analytics';
+
 @IonicPage()
 /**
  * Generated class for the AccionPage page.
@@ -17,7 +20,7 @@ import { IonicPage } from 'ionic-angular';
 export class AccionPage {
   accion:any;
   etiquetas:any;
-  constructor(private http: Http, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private http: Http, public navCtrl: NavController, public navParams: NavParams, platform: Platform,  ga: GoogleAnalytics) {
     this.accion=navParams.get("accion");
     this.http.get('assets/data/filtros.json')
       .map(res => {
@@ -32,6 +35,12 @@ export class AccionPage {
         err => console.log("error es " + err), // error
         () => console.log('Lectura de las etiquetas ' + this.etiquetas.toString()) // complete
       );
+
+    //Google Analytics
+    platform.ready().then(() => { ga.trackView("Detalle de Acción "+this.accion.numero); });
+    // this.ga.startTrackerWithId('UA-105882371-1')
+    //   .then(() => {this.ga.trackView("Detalle de Acción "+this.accion.numero);})
+    //   .catch(e => console.log('Error starting GoogleAnalytics', e));
 
   }
 
